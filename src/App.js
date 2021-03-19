0ea904c5ae3616c2dc1c3305c263cf0492fd815c
Jeremiah Johnson <jerjohnson1911@gmail.com>
2021-03-19 11:48:24 -0600

    first branch commit

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn Chakra
+              Learn Chakra test for first commit
             </Link>
           </VStack>
         </Grid>
