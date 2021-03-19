a6d10ca0625181d219c6919835c26dea90ae05a9
Drew Woodmansee <eesnamdoow@gmail.com>
2021-03-19 16:34:21 -0600

    index js in server created

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express"),
+  massive = require("massive"),
+  session = require("express-session");
+
+const { SERVER_PORT, CONNECTION_STRING, SESSION_SECRET } = process.env;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: { maxAge: null, secure: false },
+  })
+);
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+})
+  .then(db => {
+    app.set("db", db);
+    app.listen(SERVER_PORT, () => console.log(`running on ${SERVER_PORT}`));
+    console.log("db connected");
+  })
+  .catch(err => console.log(err));
