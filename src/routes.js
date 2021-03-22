2d53bf1de44390e69d320d9af84fbcb7c73d7e8e
Drew Woodmansee <eesnamdoow@gmail.com>
2021-03-22 16:05:34 -0600

    updated login

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,11 @@ import react from "react";
 import { Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import MapDark from "./components/Map/Map";
 
 export default (
   <Switch>
-    <Route exact path="/" component={Login} />
+    <Route exact path="/" component={MapDark} />
     <Route path="/login" component={Login} />
     <Route path="/register" component={Register} />
   </Switch>
