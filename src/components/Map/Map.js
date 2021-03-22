761e276fa67cd6e1ba80dcd806937acd3fee7fbf
Drew Woodmansee <eesnamdoow@gmail.com>
2021-03-22 16:28:50 -0600

    creating tables

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,93 +1,73 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import {
-    GoogleMap,
-    useLoadScript,
-    Marker,
-    // InfoWindow
-} from '@react-google-maps/api'
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  // InfoWindow
+} from "@react-google-maps/api";
 import usePlacesAutocomplete, {
-    getGeocode,
-    getLatLng
-} from 'use-places-autocomplete'
-import mapStyleLight from './mapStyleLight'
-import mapStyleDark from './mapStyleDark'
-import {
-    ChakraProvider,
-    Box,
-    Text,
-    Link,
-    VStack,
-    Code,
-    Grid,
-    theme,
-    Flex,
-    Container
-  } from '@chakra-ui/react';
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import mapStyleLight from "./mapStyleLight";
+import mapStyleDark from "./mapStyleDark";
+import { Flex } from "@chakra-ui/react";
 
-const libraries = ['places']
+const libraries = ["places"];
 const mapContainerStyle = {
-    width: '40vw',
-    height: '35vh'
-}
+  width: "40vw",
+  height: "35vh",
+};
 
 const options = {
-    styles: mapStyleDark,
-}
-
+  styles: mapStyleDark,
+};
 
 function Map() {
-    const [center, setCenter] = useState({
-        lat: 40,
-        lng: -111
-    })
-    useEffect(() => {
-        fetchLocation()
-    }, [])
+  const [center, setCenter] = useState({
+    lat: 40,
+    lng: -111,
+  });
+  useEffect(() => {
+    fetchLocation();
+  }, []);
 
-    const fetchLocation = () => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            setCenter({
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            })
-        }, _ => null)
-        
-    }
+  const fetchLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      },
+      _ => null
+    );
+  };
 
-    
-    
-    const { isLoaded, loadError } = useLoadScript({
-        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-        libraries,
-    })
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
 
+  if (loadError) return "Error loading maps!";
+  if (!isLoaded) return "Loading maps!";
 
-    if (loadError) return 'Error loading maps!'
-    if (!isLoaded) return 'Loading maps!'
-
-    
-
-
-
-    return(
-        <Flex>
-                <GoogleMap
-                    mapContainerStyle={mapContainerStyle}
-                    zoom={14}
-                    center={center}
-                    options={options}
-                    // onClick={onMapClick}
-                    // onLoad={onMapLoad}
-
-                >
-
-                    {/* <Marker
+  return (
+    <Flex>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        options={options}
+        // onClick={onMapClick}
+        // onLoad={onMapLoad}
+      >
+        {/* <Marker
                         position={{ lat: center.lat, lng: center.lng }}
                     /> */}
-
-                </GoogleMap>
-        </Flex>
-    )
+      </GoogleMap>
+    </Flex>
+  );
 }
 
-export default Map
+export default Map;
