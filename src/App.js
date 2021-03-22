381cd15976783262e1cbdb611a683ce24c5033e8
Drew Woodmansee <eesnamdoow@gmail.com>
2021-03-22 11:18:23 -0600

    completed auth pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { ChakraProvider, theme } from '@chakra-ui/react';
-import Login from './components/Login';
+import React from "react";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Login />
+      <Register />
     </ChakraProvider>
   );
 }
